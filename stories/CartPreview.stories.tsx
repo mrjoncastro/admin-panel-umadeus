ee461f2f0bb1a03fd46a51b6a58de8c7fb88e380
mrjoncastro <149547212+mrjoncastro@users.noreply.github.com>
2025-06-09 15:22:32 -0300

    feat(cart-preview): add preview modal and story

---
+++ stories/CartPreview.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import CartPreview from '../app/components/CartPreview';
+import { CartProvider } from '../lib/context/CartContext';
+import { ThemeProvider } from '../lib/context/ThemeContext';
+
+const meta = {
+  title: 'Components/CartPreview',
+  component: CartPreview,
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <CartProvider>
+          <Story />
+        </CartProvider>
+      </ThemeProvider>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof CartPreview>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
