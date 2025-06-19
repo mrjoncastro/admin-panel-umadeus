ebae85db2b2b773a49f62cae5ef598129960085b
mrjoncastro <149547212+mrjoncastro@users.noreply.github.com>
2025-06-18 21:23:20 -0300

    feat: confirm add to cart via toast

--- app/loja/produtos/[slug]/AddToCartButton.tsx
+++ app/loja/produtos/[slug]/AddToCartButton.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { useCart } from "@/lib/context/CartContext";
+import { useToast } from "@/lib/context/ToastContext";
 import type { Produto } from "@/types";
 import { ShoppingCart } from "lucide-react";
 
 export default function AddToCartButton({ produto }: { produto: Produto }) {
   const { addItem } = useCart();
+  const { showSuccess } = useToast();
+
+  const handleClick = () => {
+    addItem(produto);
+    showSuccess("Item adicionado ao carrinho!");
+  };
+
   return (
-    <button
-      onClick={() => addItem(produto)}
-      className="block w-full btn btn-primary"
-    >
+    <button onClick={handleClick} className="block w-full btn btn-primary">
       <ShoppingCart size={20} /> Adicionar ao Carrinho
     </button>
   );
