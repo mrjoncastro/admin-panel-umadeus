8990161b272e0d8b444f78648fa3617e43f8ea92
mrjoncastro <149547212+mrjoncastro@users.noreply.github.com>
2025-06-20 21:17:31 -0300

    fix(auth): load pocketbase auth from headers

--- lib/pbWithAuth.ts
+++ lib/pbWithAuth.ts
@@ -3,8 +3,8 @@ import PocketBase from 'pocketbase'
 
 export function getPocketBaseFromRequest(req: NextRequest) {
   const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL!)
-  const cookie = req.cookies.get('pb_auth')?.value
-  if (cookie) pb.authStore.loadFromCookie(`pb_auth=${cookie}`)
+  const cookieHeader = req.headers.get('cookie') || ''
+  pb.authStore.loadFromCookie(cookieHeader)
   pb.autoCancellation(false)
   return pb
 }
