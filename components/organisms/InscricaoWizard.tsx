2352fdc7d218c903dd8a62373530bee7b4ad01ba
jonatasthiago.sc <jonatasthiago.sc@gmail.com>
2025-06-21 21:35:02 -0300

    pretti

--- components/organisms/InscricaoWizard.tsx
+++ components/organisms/InscricaoWizard.tsx
@@ -21,7 +21,6 @@ interface InscricaoWizardProps {
 export default function InscricaoWizard({
   liderId,
   eventoId,
-  
 }: InscricaoWizardProps) {
   const { config } = useTenant()
   const { showSuccess, showError } = useToast()
