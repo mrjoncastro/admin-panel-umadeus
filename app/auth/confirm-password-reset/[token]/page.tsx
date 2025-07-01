3373a07a11482ba62915fcf0b79b6666a736cf94
mrjoncastro <149547212+mrjoncastro@users.noreply.github.com>
2025-06-30 21:00:43 -0300

    fix(password-reset): corrige tipo params

--- app/auth/confirm-password-reset/[token]/page.tsx
+++ app/auth/confirm-password-reset/[token]/page.tsx
@@ -1,11 +1,11 @@
 import ConfirmResetForm from '@/components/ConfirmResetForm'
 
-export default async function Page({
+export default function Page({
   params,
 }: {
-  params: Promise<{ token: string }>
+  params: { token: string }
 }) {
-  const { token } = await params
+  const { token } = params
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <ConfirmResetForm token={token} />
